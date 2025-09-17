dcc21980b854ffdaf2802dd1fc89bb3bcac3b2de
Ryan Fox <ryan.fox@softwire.com>
2025-09-17 19:46:41 +0100

    Added CSS styling

--- src/LiveBusData.tsx
+++ src/LiveBusData.tsx
@@ -64,29 +64,29 @@ function LiveBusData() {
             <h2 className="text-xl font-bold text-cyan-600 my-2">Live bus data</h2>
             
             {dataLoading && <p>Loading - please wait</p>}
-            {!dataLoading && <table className="mw-4/5 m-1 align-top py-1">
-                <thead className="border-b border-gray-400">
+            {!dataLoading && <table className="mw-4/5 m-1">
+                <thead className="border-b-2 border-gray-400">
                     <tr>
                         <th>Metric</th>
-                        <th>Statistic</th>
+                        <th className="border-l border-gray-300">Statistic</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>Number of bus stops</td>
-                        <td>{numberBusStopsString}</td>
+                        <td className = "align-text-top p-1">Number of bus stops</td>
+                        <td className = "align-text-top p-1 border-l border-gray-300">{numberBusStopsString}</td>
                     </tr>
-                    <tr>
-                        <td>Number of bus routes</td>
-                        <td>{numberBusRoutesString}</td>
+                    <tr className="border-t border-gray-300">
+                        <td className = "align-text-top p-1">Number of bus routes</td>
+                        <td className = "align-text-top p-1 border-l border-gray-300">{numberBusRoutesString}</td>
                     </tr>
-                    <tr>
-                        <td>Number of bus journeys {periodBusJourneysString}</td>
-                        <td>{numberBusJourneysString}</td>
+                    <tr className="border-t border-gray-300">
+                        <td className = "align-text-top p-1">Number of bus journeys {periodBusJourneysString}</td>
+                        <td className = "align-text-top p-1 border-l border-gray-300">{numberBusJourneysString}</td>
                     </tr>
-                    <tr>
-                        <td>Number of buses by type</td>
-                        <td>
+                    <tr className="border-t border-gray-300">
+                        <td className = "align-text-top p-1">Number of buses by type</td>
+                        <td className = "align-text-top p-1 pl-5 border-l border-gray-300">
                             <ul className="list-disc">
                                 {numberBusTypeArray.map((busTypeString) => <li>{busTypeString}</li>)}
                             </ul>
